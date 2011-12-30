532da4a98853d2caf4f28188a6af9b1172542a34
Daniel Stonier <d.stonier@gmail.com>
2011-12-30 13:35:57 +0900

    master_browser: discovery handler moved out.

--- zeroconf_android_master_browser/src/ros/zeroconf/android/jmdns/master_browser/MasterBrowserActivity.java
+++ zeroconf_android_master_browser/src/ros/zeroconf/android/jmdns/master_browser/MasterBrowserActivity.java
@@ -7,7 +7,6 @@ import javax.jmdns.ServiceInfo;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.os.AsyncTask;
-import android.os.Handler;
 import android.os.Bundle;
 import android.content.Context;
 import android.view.View;
@@ -24,6 +23,7 @@ import ros.zeroconf.jmdns.Zeroconf;
 import ros.zeroconf.jmdns.ZeroconfDiscoveryHandler;
 import ros.zeroconf.android.jmdns.Logger;
 import ros.zeroconf.android.jmdns.master_browser.R;
+import ros.zeroconf.android.jmdns.master_browser.DiscoveryHandler;
 import ros.zeroconf.android.jmdns.master_browser.DiscoveredServiceAdapter;
 import org.ros.message.zeroconf_comms.DiscoveredService;
 
@@ -31,188 +31,24 @@ import org.ros.message.zeroconf_comms.DiscoveredService;
 // adb logcat zeroconf:I *:S
 
 /**
- * This test does : 
+ * Master browser does discovery for the following services
+ * (both xxx._tcp and xxx._udp):
  * 
- * 0) publish a _ros-master._tcp service (DudeMaster)
- * 1) listens for _ros-master._tcp services
- * 2) every second it will display currently found services
- * 3) On destroy, deactivate listeners
- * 4) On destroy, remove all services
+ * - _ros-master  
+ * - _concert-master
+ * - _app-manager
  * 
- * Best way to use it is to use avahi to publish/browse on the other end, in oen shell:
+ * Easiest way to test is to use avahi to publish/browse on the other end. In a linux shell:
  * 
- * > avahi-publish -s ConcertMaster _ros-master._tcp 8883
- * 
- * In another window:
- * 
- * > avahi-browse -r _ros-master._tcp
- * 
+ * @code
+ * > avahi-publish -s DudeMaster _ros-master._tcp 8882
+ * > avahi-publish -s ConcertMaster _concert-master._tcp 8883
+ * > avahi-publish -s DudeMasterApps _app-manager._tcp 8884
+ * @endcode
+ *
+ * Then run this program on your android while it is connected to the lan.
  */
-public class MasterBrowserActivity extends Activity implements OnItemClickListener {
-
-	/*************************************************************************
-	 * Threads, Tasks and Handlers
-	 ************************************************************************/
-	
-	/*********************
-	 * Discovery Handler
-	 ********************/
-	public class DiscoveryHandler implements ZeroconfDiscoveryHandler {
-
-		/*********************
-		 * Tasks
-		 ********************/
-		private class ServiceAddedTask extends AsyncTask<DiscoveredService, String, Void> {
-			
-		    protected Void doInBackground(DiscoveredService... services) {
-		        if ( services.length == 1 ) {
-		            DiscoveredService service = services[0];
-					String result = "[+] Service added: " + service.name + "." + service.type + "." + service.domain + ".";
-					publishProgress(result);
-		        } else {
-		            publishProgress("Error - ServiceAddedTask::doInBackground received #services != 1");
-		        }
-		        return null;
-		    }
-
-		    protected void onProgressUpdate(String... progress) {
-		    	uiLog(progress);
-			}
-		}
-
-		private class ServiceResolvedTask extends AsyncTask<DiscoveredService, String, DiscoveredService> {
-			
-		    protected DiscoveredService doInBackground(DiscoveredService... services) {
-		        if ( services.length == 1 ) {
-		            DiscoveredService discovered_service = services[0];
-			    	String result = "[=] Service resolved: " + discovered_service.name + "." + discovered_service.type + "." + discovered_service.domain + ".\n";
-			    	result += "    Port: " + discovered_service.port;
-			    	for ( String address : discovered_service.ipv4_addresses ) {
-			    		result += "\n    Address: " + address;
-			    	}
-			    	for ( String address : discovered_service.ipv6_addresses ) {
-			    		result += "\n    Address: " + address;
-			    	}
-			    	publishProgress(result);
-					int index = 0;
-					for ( DiscoveredService s : discovered_services ) {
-						if ( s.name.equals(discovered_service.name) ) {
-							break;
-						} else {
-							++index;
-						}
-					}
-					if ( index == discovered_services.size() ) {
-						return discovered_service;
-					} else {
-						android.util.Log.i("zeroconf", "Tried to add an existing service (fix this)");
-					}
-		        } else {
-		            publishProgress("Error - ServiceAddedTask::doInBackground received #services != 1");
-		        }
-		        return null;
-		    }
-
-		    protected void onProgressUpdate(String... progress) {
-		    	uiLog(progress);
-			}
-		    
-		    protected void onPostExecute(DiscoveredService discovered_service) {
-		    	// add to the content and notify the list view if its a new service
-		    	if ( discovered_service != null ) {
-					discovered_services.add(discovered_service);
-					adapter.notifyDataSetChanged();
-		    	}
-		    }
-		}
-		
-		private class ServiceRemovedTask extends AsyncTask<DiscoveredService, String, DiscoveredService> {
-			
-		    protected DiscoveredService doInBackground(DiscoveredService... services) {
-		        if ( services.length == 1 ) {
-		            DiscoveredService discovered_service = services[0];
-		            String result = "[-] Service removed: " + discovered_service.name + "." + discovered_service.type + "." + discovered_service.domain + ".\n";			    	result += "    Port: " + discovered_service.port;
-			    	publishProgress(result);
-			    	return discovered_service;
-		        } else {
-		            publishProgress("Error - ServiceAddedTask::doInBackground received #services != 1");
-		        }
-		        return null;
-		    }
-
-		    protected void onProgressUpdate(String... progress) {
-		    	uiLog(progress);
-			}
-		    
-		    protected void onPostExecute(DiscoveredService discovered_service) {
-		    	// remove service from storage and notify list view
-		    	if ( discovered_service != null ) {
-					int index = 0;
-					for ( DiscoveredService s : discovered_services ) {
-						if ( s.name.equals(discovered_service.name) ) {
-							break;
-						} else {
-							++index;
-						}
-					}
-					if ( index != discovered_services.size() ) {
-						discovered_services.remove(index);
-						discovery_adapter.notifyDataSetChanged();
-					} else {
-						android.util.Log.i("zeroconf", "Tried to remove a non-existant service");
-					}
-		    	}
-		    }
-		}
-
-		/*********************
-		 * Variables
-		 ********************/
-		private ArrayList<DiscoveredService> discovered_services;
-	    private DiscoveredServiceAdapter adapter;
-		private TextView text_view;
-
-		/*********************
-		 * Constructors
-		 ********************/
-		public DiscoveryHandler(TextView tv, DiscoveredServiceAdapter discovery_adapter, ArrayList<DiscoveredService> discovered_services) {
-			this.text_view = tv;
-			this.adapter = discovery_adapter;
-			this.discovered_services = discovered_services;
-		}
-
-		/*********************
-		 * Callbacks
-		 ********************/
-		public void serviceAdded(DiscoveredService service) {
-			new ServiceAddedTask().execute(service);
-		}
-		
-		public void serviceRemoved(DiscoveredService service) {
-			new ServiceRemovedTask().execute(service);
-		}
-		
-		public void serviceResolved(DiscoveredService service) {
-			new ServiceResolvedTask().execute(service);
-		}
-
-		/*********************
-		 * Utility
-		 ********************/
-		private void uiLog(String... messages) {
-	        for (String msg : messages ) {
-	            android.util.Log.i("zeroconf", msg);
-	            text_view.append(msg + "\n");
-	    	}
-	    	int line_count = text_view.getLineCount(); 
-	    	int view_height = text_view.getHeight();
-	    	int pixels_per_line = text_view.getLineHeight();
-	    	int pixels_difference = line_count*pixels_per_line - view_height;
-	    	if ( pixels_difference > 0 ) {
-	    		text_view.scrollTo(0, pixels_difference);
-	    	}
-		}
-	}
+public class MasterBrowserActivity extends Activity {
 
 	private void scrollToBottom() {
     	int line_count = tv.getLineCount(); 
@@ -222,15 +58,6 @@ public class MasterBrowserActivity extends Activity implements OnItemClickListen
     	if ( pixels_difference > 0 ) {
     		tv.scrollTo(0, pixels_difference);
     	}
-	}
-
-	/*******************
-	 * Gui Callbacks
-	 *******************/
-
-	public void onItemClick(AdapterView adapter_view, View view, int position, long id) {
-		android.util.Log.i("zeroconf", "You clicked the list");
-		Toast.makeText(this, "You clicked entry #" + position ,Toast.LENGTH_LONG).show();
 	}
 	
 	/********************
@@ -297,7 +124,6 @@ public class MasterBrowserActivity extends Activity implements OnItemClickListen
         discovered_services = new ArrayList<DiscoveredService>();
         setContentView(R.layout.main);
         lv = (ListView)findViewById(R.id.discovered_services_view);
-        lv.setOnItemClickListener(this);
         discovery_adapter = new DiscoveredServiceAdapter(this, discovered_services);
         lv.setAdapter(discovery_adapter);
         tv = (TextView)findViewById(R.id.mytextview);
